1c79f6c4be0d11900d3f03265434040f3b1f9af2
olyv <a.n.lyvin@gmail.com>
2017-09-26 16:43:08 +0200

    update capabilities

--- src/test/java/com/olyv/wortschatz/appium/BaseTest.java
+++ src/test/java/com/olyv/wortschatz/appium/BaseTest.java
@@ -2,7 +2,6 @@ package com.olyv.wortschatz.appium;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -10,28 +9,45 @@ import org.testng.annotations.BeforeClass;
 import java.io.File;
 import java.net.URL;
 
-public class BaseTest
-{
+import static org.openqa.selenium.remote.DesiredCapabilities.android;
+
+public class BaseTest {
+
+    private static final String PATH_TO_APK = "./src/main/resources";
+    private static final String APK_NAME = "app-debug.apk";
+    private static final String DEVICE_NAME_CAPABILITY = "app-debug.apk";
+    private static final String DEVICE_NAME_VALUE = "Nexus 5X API 26";
+    private static final String PLATFORM_NAME_CAPABILITY = "platformName";
+    private static final String PLATFORM_NAME_VALUE = "android";
+    private static final String PLATFORM_VERSION_CAPABILITY = "platformVersion";
+    private static final String PLATFORM_VERSION_VERSION = "8";
+    private static final File APPLICATION_FILE = new File(PATH_TO_APK, APK_NAME);
+    private static final String APP_PACKAGE_CAPABILITY = "appPackage";
+    private static final String APP_PACKAGE_VALUE = "com.olyv.wortschatz.ui";
+    private static final String APP_ACTIVITY_CAPABILITY = "appActivity";
+    private static final String APP_ACTIVITY_VALUE = ".StartActivity";
+    private static final String DRIVER_REMOTE_ADDRESS = "http://127.0.0.1:4723/wd/hub";
+    private static final String APPLICATION_FILE_CAPABILITY = "app";
     protected AppiumDriver driver;
 
     @BeforeClass
-    public void setUp() throws Exception
-    {
-        File app = new File("./src/main/resources", "app-debug.apk");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities().android();
-        capabilities.setCapability("deviceName", "foo");//"Nexus 4");
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "4.4");
-        capabilities.setCapability("app", app.getAbsolutePath());
-        capabilities.setCapability("appPackage", "com.olyv.wortschatz.ui");
-        capabilities.setCapability("appActivity", ".StartActivity");
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    public void setUp() throws Exception {
+        driver = new AndroidDriver(new URL(DRIVER_REMOTE_ADDRESS), getAndroidCapabilities());
     }
 
     @AfterClass
-    public void tearDown() throws Exception
-    {
+    public void tearDown() throws Exception {
         driver.quit();
+    }
+
+    private DesiredCapabilities getAndroidCapabilities() {
+        DesiredCapabilities capabilities = android();
+        capabilities.setCapability(DEVICE_NAME_CAPABILITY, DEVICE_NAME_VALUE);
+        capabilities.setCapability(PLATFORM_NAME_CAPABILITY, PLATFORM_NAME_VALUE);
+        capabilities.setCapability(PLATFORM_VERSION_CAPABILITY, PLATFORM_VERSION_VERSION);
+        capabilities.setCapability(APPLICATION_FILE_CAPABILITY, APPLICATION_FILE.getAbsolutePath());
+        capabilities.setCapability(APP_PACKAGE_CAPABILITY, APP_PACKAGE_VALUE);
+        capabilities.setCapability(APP_ACTIVITY_CAPABILITY, APP_ACTIVITY_VALUE);
+        return capabilities;
     }
 }
