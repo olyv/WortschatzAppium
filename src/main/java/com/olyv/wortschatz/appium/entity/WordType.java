0be44b4468e51413a747dadeae0e9d98f62e01c8
lyvina <lyvina@ryanair.com>
2019-08-05 19:05:39 +0200

    add mapping for word type to displayed in UI spinner value

--- src/main/java/com/olyv/wortschatz/appium/entity/WordType.java
+++ src/main/java/com/olyv/wortschatz/appium/entity/WordType.java
@@ -2,7 +2,17 @@ package com.olyv.wortschatz.appium.entity;
 
 public enum WordType {
 
-    ADJECTIVE,
-    VERB,
-    NOUN
+    ADJECTIVE("Translation"),
+    VERB("Verb"),
+    NOUN("Noun");
+
+    String spinnerValueInEditor;
+
+    WordType(String spinnerValue) {
+        this.spinnerValueInEditor = spinnerValue;
+    }
+
+    public String getSpinnerValueInEditor() {
+        return spinnerValueInEditor;
+    }
 }
